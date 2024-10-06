f46878b243aa7d5ecb923146e82c625f4b8f7e6b
Restyled.io <commits@restyled.io>
2024-10-06 12:48:15 +0000

    Restyled by prettier

--- app/page.jsx
+++ app/page.jsx
@@ -33,7 +33,7 @@ export default function Home() {
 
       const days = Math.floor(diff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
-        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
       );
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
